afd8f68499b60f8a52b394ec66ed96fed04a5ebf
XCode101 <76824572+SomeoneSketchySync@users.noreply.github.com>
2021-07-02 00:28:54 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "libs/String.cpp"
+
+int main() {
+	std::string input = "func ((it Works!chuck));";
+  std::cout << input << "\n";
+	int buffer [] = {0,1,2,3,4,5};
+	std::cout << buffer << "\n";
+}
